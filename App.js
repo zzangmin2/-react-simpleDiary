872a5f9d48b561a8afff46e1298e2d593b937135
jungmin <103804129+zzangmin2@users.noreply.github.com>
2023-02-12 22:13:33 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,37 +1,55 @@
+import React, {useRef, useState} from "react";
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-const dummyList = [
-  {
-    id: 1,
-    author: "짱민1",
-    content: "하이 1",
-    emotion: 5,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 2,
-    author: "짱민2",
-    content: "하이 2",
-    emotion: 4,
-    created_date: new Date().getTime()
-  },
-  {
-    id: 3,
-    author: "짱민3",
-    content: "하이 3",
-    emotion: 3,
-    created_date: new Date().getTime()
-  },
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "짱민1",
+//     content: "하이 1",
+//     emotion: 5,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 2,
+//     author: "짱민2",
+//     content: "하이 2",
+//     emotion: 4,
+//     created_date: new Date().getTime()
+//   },
+//   {
+//     id: 3,
+//     author: "짱민3",
+//     content: "하이 3",
+//     emotion: 3,
+//     created_date: new Date().getTime()
+//   },
 
-]
+// ]
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const dateId = useRef(0);
+
+  const onCreate = (author,content,emotion) =>{
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dateId.current,
+    };
+    dateId.current += 1;
+    setData([newItem, ...data]);
+  }
+
   return (
     <div className = "App">
-      <DiaryEditor />
-      <DiaryList diaryList = {dummyList}/>
+      <DiaryEditor onCreate ={onCreate}/>
+      <DiaryList diaryList = {data}/>
     </div>
   );
 }
