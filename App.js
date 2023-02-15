299c8c7fc8540db5a26682c50545fc513c80c3d1
jungmin <103804129+zzangmin2@users.noreply.github.com>
2023-02-15 14:23:05 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, {useRef, useState} from "react";
-import './App.css';
-import DiaryEditor from './DiaryEditor';
-import DiaryList from './DiaryList';
+import React, { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import Lifecycle from "./Lifecycle";
 
 // const dummyList = [
 //   {
@@ -33,23 +34,37 @@ function App() {
 
   const dateId = useRef(0);
 
-  const onCreate = (author,content,emotion) =>{
+  const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
     const newItem = {
       author,
       content,
       emotion,
       created_date,
-      id : dateId.current,
+      id: dateId.current,
     };
     dateId.current += 1;
     setData([newItem, ...data]);
-  }
+  };
 
+  const onRemove = (targetId) => {
+    console.log(targetId + "가 삭제되었습니다.");
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
   return (
-    <div className = "App">
-      <DiaryEditor onCreate ={onCreate}/>
-      <DiaryList diaryList = {data}/>
+    <div className="App">
+      <Lifecycle />
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
 }
