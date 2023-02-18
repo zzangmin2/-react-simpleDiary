06bcd118d4efe7d659b0f70c03e36c67211b0f0a
jungmin <103804129+zzangmin2@users.noreply.github.com>
2023-02-18 15:30:11 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,38 +1,35 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "짱민1",
-//     content: "하이 1",
-//     emotion: 5,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id: 2,
-//     author: "짱민2",
-//     content: "하이 2",
-//     emotion: 4,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id: 3,
-//     author: "짱민3",
-//     content: "하이 3",
-//     emotion: 3,
-//     created_date: new Date().getTime()
-//   },
+//https://jsonplaceholder.typicode.com/comments
 
-// ]
-
-function App() {
+const App = () => {
   const [data, setData] = useState([]);
 
   const dateId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dateId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -62,11 +59,10 @@ function App() {
   };
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
-}
+};
 
 export default App;
